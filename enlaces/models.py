65953a913465959cb0fc5f81d242fe017936b5f3
EstebanBust <esteban.bustamante.c@gmail.com>
2023-04-20 12:20:54 -0400

    third commit

--- enlaces/models.py
+++ enlaces/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import datetime
 
 class Enlace(models.Model):
@@ -7,4 +8,11 @@ class Enlace(models.Model):
     image = models.ImageField(upload_to="static/img")
     url = models.URLField(blank=True)
     date = models.DateField(datetime.date.today)
+    
+class MyUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Agrega aquí cualquier otro campo adicional que desees para tu usuario
+
+    def __str__(self):
+        return self.user.username
     
--- enlaces/views.py
+++ enlaces/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from .models import Enlace
+from .models import Enlace, MyUser
+from django.contrib.auth.models import User
 
 # Create your views here.
 def enlaces(request):
     enlaces = Enlace.objects.all()
     return render(request, "home.html", {"enlaces": enlaces})
+
+def create_superuser(request):
+    # Crea el usuario
+    user = User.objects.create_superuser(
+        username='Luktor',
+        password='1n-o$XK>u7l@hK0',
+        email='esteban.bustamante.c@gmail.com'
+    )
+
+    # Crea la instancia de MyUser asociada al usuario
+    myuser = MyUser.objects.create(user=user)
+
+    # Agrega cualquier otra configuración que desees para el usuario aquí
+
+    # Renderiza una respuesta para indicar que el usuario ha sido creado exitosamente
+    return render(request, 'create_superuser.html')
